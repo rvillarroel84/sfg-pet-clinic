055923bb873c4bec6c685b89e1c55148a8fdb507
Ronny <ronaldovm.villarroel@gmail.com>
2019-12-21 11:00:34 -0400

    added specialties, added to Vet closes #27

--- pet-clinic-data/src/main/java/com/villarroel/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/villarroel/sfgpetclinic/model/Vet.java
@@ -1,5 +1,16 @@
 package com.villarroel.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Speciality> specialties;
+
+    public Set<Speciality> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Speciality> specialties) {
+        this.specialties = specialties;
+    }
 }
