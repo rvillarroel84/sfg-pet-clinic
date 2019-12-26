cf841b43fcdb0623ed465513f4fab0b711e00721
Ronny Villarroel <rvillarroel@avicolasofia.com>
2019-12-26 14:12:33 -0400

    Fixed Broken Links. Closes #45

--- pet-clinic-web/src/main/java/com/villarroel/sfgpetclinic/bootstrap/DataLoad.java
+++ pet-clinic-web/src/main/java/com/villarroel/sfgpetclinic/bootstrap/DataLoad.java
@@ -1,21 +1,28 @@
 package com.villarroel.sfgpetclinic.bootstrap;
 
 import com.villarroel.sfgpetclinic.model.Owner;
+import com.villarroel.sfgpetclinic.model.Pet;
+import com.villarroel.sfgpetclinic.model.PetType;
 import com.villarroel.sfgpetclinic.model.Vet;
 import com.villarroel.sfgpetclinic.services.OwnerService;
+import com.villarroel.sfgpetclinic.services.PetTypeService;
 import com.villarroel.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoad implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoad(OwnerService ownerService, VetService vetService) {
+    public DataLoad(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
 //    public DataLoad() {
@@ -26,16 +33,45 @@ public class DataLoad implements CommandLineRunner {
 
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args){
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatType = petTypeService.save(cat);
 
         Owner owner = new Owner();
         owner.setFirstName("Ronny");
         owner.setLastName("Villarroel");
+        owner.setAddress("492 Changos");
+        owner.setCity("Santa Cruz");
+        owner.setTelephone("75563350");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogType);
+        mikesPet.setOwner(owner);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Yoy");
+        owner.getPets().add(mikesPet);
         ownerService.save(owner);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Anahi");
         owner2.setLastName("Villarroel");
+        owner2.setAddress("492 Pirai");
+        owner2.setCity("Beni");
+        owner2.setTelephone("65563350");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Just Cat");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatType);
+        owner2.getPets().add(fionasCat);
+
         ownerService.save(owner2);
 
 //        System.out.println("Carga de Due;os");
