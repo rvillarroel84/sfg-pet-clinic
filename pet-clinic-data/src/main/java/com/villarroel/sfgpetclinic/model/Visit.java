1764ec4ad4062b8bc769f9607d215361869c149a
Ronny Villarroel <rvillarroel@avicolasofia.com>
2020-01-02 14:18:59 -0400

    Added Visit Entity, Closes #38

--- pet-clinic-data/src/main/java/com/villarroel/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/villarroel/sfgpetclinic/model/Visit.java
@@ -1,10 +1,18 @@
 package com.villarroel.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
+    @Column(name = "name")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
