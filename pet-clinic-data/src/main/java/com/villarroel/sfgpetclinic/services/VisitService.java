77e5c2beba7874b292949bc68cd88d130fe9ecc3
Ronny Villarroel <rvillarroel@avicolasofia.com>
2020-01-06 14:28:52 -0400

    Implemented Map impl for Visit Service, Closes #46

---
+++ pet-clinic-data/src/main/java/com/villarroel/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.villarroel.sfgpetclinic.services;
+
+import com.villarroel.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/villarroel/sfgpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,38 @@
+package com.villarroel.sfgpetclinic.services.map;
+
+import com.villarroel.sfgpetclinic.model.Visit;
+import com.villarroel.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() ==null
+                || visit.getPet().getId() ==null || visit.getPet().getOwner() ==null){
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
